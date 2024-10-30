1d4a5652bb301a9c89230634786752bbde2319a1
tanyavalko <tanyavalko25@gmail.com>
2024-10-30 18:25:59 +0000

    feat: change title on dev

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Main</header>
-      <header className="App-header">Develop new deploy</header>
-      <header className="App-header">Develop new deploy 1</header>
+      <header className="App-header">This is Develop</header>
     </div>
   );
 }
