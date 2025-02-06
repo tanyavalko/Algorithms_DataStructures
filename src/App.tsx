df02c07d0c7c51ea9ccfff4c162f81de78951edd
tanyavalko <tanyavalko25@gmail.com>
2025-02-06 23:04:45 +0000

    feat: add changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { DeployOnDevComp } from './devDeploy';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">This is Develop</header>
+      <DeployOnDevComp />
     </div>
   );
 }
