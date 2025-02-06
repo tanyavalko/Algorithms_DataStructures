588c738fc601929ad3a40f190009ed6994f399d5
tanyavalko <tanyavalko25@gmail.com>
2025-02-06 23:43:42 +0000

    update workflow

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">This is Develop</header>
-      <div>Some texttst</div>
+      <div className="App-tex">Some texttst</div>
     </div>
   );
 }
