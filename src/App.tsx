b45f331b4b86b0b82717987f7292b872295cc6f9
tanyavalko <tanyavalko25@gmail.com>
2025-02-06 23:33:57 +0000

    update text

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
-import { DeployOnDevComp } from './devDeploy';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">This is Develop</header>
-      <DeployOnDevComp />
       <div>Some text</div>
     </div>
   );
--- src/devDeploy/index.tsx
+++
@@ -1,3 +0,0 @@
-export const DeployOnDevComp = () => {
-  return <div>It deployed on Devv</div>;
-};
