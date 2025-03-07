8f99379c08f8f08fb53726dab36b8a0deddae977
tanyavalko <tanyavalko25@gmail.com>
2025-03-07 09:44:32 +0000

    fix: new text formain

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">This is Develop</header>
-      <header className="App-header">That new text for Main</header>
+      <header className="App-header">That new text for Main 2</header>
     </div>
   );
 }
